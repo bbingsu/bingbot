bbb549914facc334ea7f31866d42a6f8c6a7cc56
SNMHZ <snmhz325@gmail.com>
2022-02-11 00:49:29 +0900

    feat: Update random command

--- commands/basic.py
+++ commands/basic.py
@@ -1,3 +1,5 @@
+import os
+import random
 import discord
 from discord.ext.commands import Context
 import discord.ext.commands as commands
@@ -23,6 +25,9 @@ async def 이몸등장(ctx: Context):
     embed.set_footer(text='먀옹')
     await ctx.channel.send(embed=embed)
 
+IMG_PATH = os.path.join(os.path.dirname(__file__), '../image')
+files = os.listdir(IMG_PATH)
+IMG_URL = 'https://raw.githubusercontent.com/bbingsu/bingbot/main/image/'
 introduce = discord.Embed(title='나는 빙수다냥',
                           description="이렇게 사용한다냥!\n`빙수 안녕`, `@BINGSU 안녕`")
 introduce.add_field(name='대화', value='안녕\r\n바보\r\n기여')
@@ -30,19 +35,30 @@ introduce.add_field(name='게임', value='홀짝')
 introduce.add_field(name='사진', value='이몸등장\r\n\랜덤')
 introduce.add_field(name='노래', value='입장\r\n\틀어\r\n\멈춰\r\n\퇴장')
 introduce.add_field(name='기타', value='번역')
-introduce.set_thumbnail(url='http://snmhz325.asuscomm.com')
+introduce.set_thumbnail(url='https://raw.githubusercontent.com/bbingsu/bingbot/main/image/merry.jpg')
 introduce.set_footer(text='명령어 아이디어 제공은 환영한다냥!')
 
 @commands.command()
 async def 소개(ctx: Context):
+    introduce.set_thumbnail(url=IMG_URL + random.choice(files))
     await ctx.channel.send(embed=introduce)
 
 @commands.command()
 async def 소개해봐(ctx: Context):
+    introduce.set_thumbnail(url=IMG_URL + random.choice(files))
     await ctx.channel.send(embed=introduce)
 
 @commands.command()
 async def 자기소개(ctx: Context):
+    introduce.set_thumbnail(url=IMG_URL + random.choice(files))
     await ctx.channel.send(embed=introduce)
 
-basicCmd = [ 안녕, 바보, 기여, 이몸등장, 소개, 소개해봐, 자기소개 ]
+@commands.command()
+async def 랜덤(ctx: Context):
+    embed = discord.Embed(title='',
+                          description='')
+    embed.set_image(url=IMG_URL + random.choice(files))
+    embed.set_footer(text='먀옹')
+    await ctx.channel.send(embed=embed)
+
+basicCmd = [ 안녕, 바보, 기여, 이몸등장, 소개, 소개해봐, 자기소개, 랜덤 ]
