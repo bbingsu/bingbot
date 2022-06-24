a96af5445536e420e318dbabf7868a133a06ccd0
SNMHZ <snmhz325@gmail.com>
2022-06-25 00:25:28 +0900

    feat: Add command `랜덤포지션`

--- commands/etc.py
+++ commands/etc.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext.commands import Context
 import discord.ext.commands as commands
 
+import random
+
 from module.translation import translate_google
 
 @commands.command()
@@ -9,4 +11,25 @@ async def 번역(ctx: Context, *, text: str):
     """구글 번역 api를 이용해 한국어를 영어로 번역합니다."""
     await ctx.channel.send(translate_google(text, "ko", "en"))
 
-etcCmd = [ 번역 ]
+position = ["멘탱", "섭탱", "멘딜", "섭딜", "멘힐", "섭힐"]
+
+@commands.command()
+async def 랜덤포지션(ctx: Context, *, text:str):
+    """
+    6명의 오버워치 포지션을 랜덤으로 결정합니다.
+    """
+    try:
+        users = text.split()
+        if len(users) < 6:
+            while len(users) < 6:
+                users.append(str(len(users)+1))
+        random.shuffle(users)
+        message = ""
+        for pos, user in zip(position, users):
+            message += pos + " = " + user + "\n"
+        await ctx.channel.send(message)
+    except:
+        await ctx.channel.send("다시 시도해보라냥")
+
+
+etcCmd = [ 번역, 랜덤포지션 ]
